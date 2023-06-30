4fa63fa72d3075bcac29d7fee45627a2ca508e40
Adan28742 <20101034@ue.edu.pe>
2023-06-30 18:38:43 -0500

    Se mejor Galeria

--- src/pages/Galeria.vue
+++ src/pages/Galeria.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="galery-container">
     <div class="centrado">
-      <div style="text-align: right">
-        <button class="Inicio-button" @click="irAlDashboard">Inicio</button>
-      </div>
-      <h2>Galeria</h2>
+      <h2 class="text_gale">Galeria</h2>
       <div class="galery">
         <div
           v-for="galeria in galerias"
@@ -27,32 +24,32 @@ export default {
         {
           id: 1,
           imagen:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIQD6L4AymYJXgDPHFzR2b86nH2A4IebA7JA&usqp=CAU",
+            "https://cdn.forbes.com.mx/2021/12/hoteles-de-lujo-tres-e1639513889195-640x360.jpg",
         },
         {
           id: 2,
           imagen:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIQD6L4AymYJXgDPHFzR2b86nH2A4IebA7JA&usqp=CAU",
+            "https://i.blogs.es/9bfb99/intercontinental-barcelona_salones/1366_2000.jpeg",
         },
         {
           id: 3,
           imagen:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIQD6L4AymYJXgDPHFzR2b86nH2A4IebA7JA&usqp=CAU",
+            "https://www.eltiempo.com/files/article_multimedia/uploads/2018/04/06/5ac7ef6428291.jpeg",
         },
         {
           id: 4,
           imagen:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIQD6L4AymYJXgDPHFzR2b86nH2A4IebA7JA&usqp=CAU",
+            "https://www.somosperu.org.pe/wp-content/uploads/2017/09/Hotel-Hilton-Lima-Miraflores.jpg",
         },
         {
           id: 5,
           imagen:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIQD6L4AymYJXgDPHFzR2b86nH2A4IebA7JA&usqp=CAU",
+            "https://i0.wp.com/tucochinito.com/wp-content/uploads/2019/09/cuarto-lujo-hotel-1.jpg",
         },
         {
           id: 6,
           imagen:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIQD6L4AymYJXgDPHFzR2b86nH2A4IebA7JA&usqp=CAU",
+            "https://www.ceupe.com/images/easyblog_articles/2539/b2ap3_large_Caractersticas-de-las-instalaciones-hoteleras.jpg",
         },
       ],
     };
@@ -62,10 +59,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
 .centrado {
   text-align: center;
 }
-
+.text_gale {
+  font-family: "Exo";
+  color: white;
+  font-weight: 520px;
+}
 .galery {
   display: flex;
   margin-bottom: 40px;
@@ -77,7 +79,8 @@ export default {
 .imagen-container {
   box-sizing: border-box;
   padding: 5px;
-  width: 30%;
+  width: 40%;
+  height: 100%;
 }
 
 .imagen {
@@ -99,7 +102,7 @@ export default {
 }
 
 .galery-container {
-  max-width: 800px;
+  max-width: 1700px;
   margin: 0 auto;
   padding: 40px;
   font-family: "Poppins", sans-serif;
