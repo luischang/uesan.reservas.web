8e992fe0357a7a8c03752c472871ce603c1b6155
Jeferson Sandoval <20100714@ue.edu.pe>
2023-06-30 14:16:07 -0500

    Se actualizo la pagina seleccionar servicios

--- src/components/Reserva/SeleccionarServiciosAdicionales.vue
+++ src/components/Reserva/SeleccionarServiciosAdicionales.vue
@@ -1,99 +1,90 @@
 <template>
   <div class="modal">
-    <div class="servicio-container">
-      <h2>Servicios Disponibles</h2>
-
-      <q-btn class="salir_pos" @click="closeModal">Terminar</q-btn>
-
-      <div v-for="servicio in servicios" :key="servicio.id" class="servicio">
-        <div class="servicio-details">
-          <h3>{{ servicio.nombre }}</h3>
-          <p>{{ servicio.descripcion }}</p>
-          <p>Precio: {{ servicio.precio }}</p>
-          <button @click="agregarAlCarrito(servicio)">Agregar</button>
-        </div>
-      </div>
-
-      <div class="carrito-icon" @click="mostrarCarrito = !mostrarCarrito">
-        <button class="carrito-button">
-          <img
-            class="carrito-image"
-            src="https://images.vexels.com/media/users/3/200097/isolated/preview/942820836246f08c2d6be20a45a84139-icono-de-carrito-de-compras-carrito-de-compras.png"
-            alt="Carrito de compras"
-          />
-          <span class="carrito-count">{{ carrito.length }}</span>
+    <div class="filters">
+      <select v-model="filtro">
+        <option value="todos" selected>Todos</option>
+        <option value="disponible">Disponible</option>
+        <option value="ocupado">Ocupado</option>
+      </select>
+    </div>
+    <div class="servicio" v-for="servicio in serviciosFiltrados" :key="servicio.id">
+      <div
+        :class="{
+          'servicio-disponible': servicio.estado,
+          'servicio-ocupado': !servicio.estado,
+          'servicio-seleccionado': servicioSeleccionado === servicio.id,
+        }"
+      >
+        <h3>{{ servicio.descripcion }}</h3>
+        <p>Estado: {{ servicio.estado ? "Disponible" : "Ocupado" }}</p>
+        <p>Precio: {{ servicio.precio }}</p>
+        <button
+          v-if="servicio.estado"
+          @click="seleccionarServicio(servicio.id)"
+          class="style_btn"
+        >
+          Seleccionar
         </button>
-      </div>
-
-      <div class="carrito-overlay" v-if="mostrarCarrito">
-        <div class="carrito-container">
-          <h3>Carrito de compras</h3>
-          <button class="cerrar-carrito" @click="mostrarCarrito = false">
-            Cerrar
-          </button>
-          <div v-for="item in carrito" :key="item.id" class="carrito-item">
-            <p>{{ item.descripcion }}</p>
-            <p>Precio: {{ item.precio }}</p>
-          </div>
-          <!-- <button class="confirmar-compra" @click="confirmarCompra">
-            Confirmar compra
-          </button> -->
-        </div>
       </div>
     </div>
   </div>
+  <div>
+    <q-btn class="terminar-btn" @click="closeModal">TERMINAR</q-btn>
+  </div>
 </template>
 
-<script>
-import axios from "axios";
 
+<script>
 export default {
   emits: ["close-modal"],
   data() {
     return {
       servicios: [],
-      carrito: [],
-      mostrarCarrito: false,
+      filtro: "todos",
+      servicioSeleccionado: null,
     };
   },
-  methods: {
-    agregarAlCarrito(servicio) {
-      this.carrito.push(servicio);
-      const listaCarrito =
-        JSON.parse(localStorage.getItem("ServiciosAdicionales")) || [];
-
-      // Agregar el idServicio a la lista
-      listaCarrito.push(servicio.idServicio);
-
-      // Guardar la lista actualizada en el localStorage
-      localStorage.setItem(
-        "ServiciosAdicionales",
-        JSON.stringify(listaCarrito)
-      );
+  mounted() {
+    fetch("http://localhost:5023/api/v1/Servicio")
+      .then((response) => response.json())
+      .then((data) => {
+        this.servicios = data;
+      })
+      .catch((error) => {
+        console.error("Error al obtener los servicios:", error);
+      });
+  },
+  computed: {
+    serviciosFiltrados() {
+      if (this.filtro === "todos") {
+        return this.servicios;
+      } else if (this.filtro === "disponible") {
+        return this.servicios.filter((servicio) => servicio.estado);
+      } else if (this.filtro === "ocupado") {
+        return this.servicios.filter((servicio) => !servicio.estado);
+      }
+      return this.servicios;
     },
-    obtenerServicios() {
-      const apiUrl = "http://localhost:5023/api/v1/Servicio";
-
-      axios
-        .get(apiUrl)
-        .then((response) => {
-          this.servicios = response.data;
-        })
-        .catch((error) => {
-          console.error("Error al obtener los servicios:", error);
-        });
+  },
+  methods: {
+    seleccionarServicio(id) {
+      if (this.servicioSeleccionado === id) {
+        // Si el servicio ya está seleccionado, lo deseleccionamos
+        this.servicioSeleccionado = null;
+      } else {
+        // Si se selecciona un nuevo servicio, actualizamos el servicio seleccionado
+        this.servicioSeleccionado = id;
+      }
     },
     closeModal() {
       this.$emit("close-modal");
     },
   },
-  mounted() {
-    this.obtenerServicios();
-  },
 };
 </script>
 
-<style scoped>
+
+<style>
 @import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
 
 .modal {
@@ -106,167 +97,67 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: auto;
 }
-.servicio-container {
-  max-width: 500px;
-  margin: 0 auto;
-  font-family: "Exo";
-  color: white;
-  background-color: #eabe7c;
-  margin-top: 8%;
-  overflow: auto;
-}
-
 .servicio {
-  display: flex;
-  margin-bottom: 40px;
-  align-items: center;
+  display: inline-block;
+  width: 20%;
+  padding: 10px;
+  box-sizing: border-box;
+  margin: 10px;
 }
 
-.servicio-details {
-  color: #fff;
-  width: 300px;
-  margin-left: 20px;
-}
-
-.carrito-container {
-  position: absolute;
-  width: 300px;
-  overflow: auto;
-}
-
-h2 {
-  font-weight: bold;
+.servicio-disponible {
+  background-color: #d48208;
+  border-radius: 5px;
+  font-family: "Exo";
   text-align: center;
   color: white;
-  font-size: 24px;
-  margin-bottom: 20px;
+  border: white 1px solid;
 }
-
-h3 {
-  font-weight: bold;
+.servicio-seleccionado {
+  background-color: rgb(
+    75,
+    149,
+    75
+  ); /* Aquí puedes definir el color de fondo que desees */
+}
+.servicio-ocupado {
+  background-color: #d48208;
+}
+.filters {
+  margin-top: 20px;
+  font-size: large;
+  font-family: "Exo";
+  max-width: 15%;
+  border-color: transparent;
+}
+.style_btn {
+  padding: 8px 14px;
+  background-color: #d48208;
   color: white;
-  font-size: 20px;
-  margin-bottom: 10px;
-}
-
-p {
-  margin-bottom: 18px;
-  color: black;
-  line-height: 1.6;
-  text-align: justify;
-}
-
-button {
-  background-color: #e06e43;
-  color: #fff;
   border: none;
-  padding: 8px 16px;
-  font-size: 16px;
+  border-radius: 2px;
   cursor: pointer;
+  font-size: 15px;
+  font-weight: 15px;
+  font-family: "Exo";
+  position: absolute;
+  margin-left: -50px;
+  margin-top: 20px;
 }
-button:hover {
+.style_btn:hover {
   background-color: #d9dd92;
   color: black;
 }
-
-.home-button {
+.terminar-btn {
   position: absolute;
-  top: 60px;
-  right: 20px;
-  background-color: #e06e43;
-  color: #fff;
-  border: none;
-  padding: 8px 16px;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-.carrito-item {
-  margin-bottom: 10px;
-  padding: 10px;
-  background-color: #fff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 8px;
-}
-
-.carrito-icon {
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  background-color: #e06e43;
-  color: #fff;
-  border: none;
-  padding: 8px 16px;
-  font-size: 16px;
-  cursor: pointer;
-  border-radius: 50%;
+  bottom: -150px;
+  left: 405px;
+  font-family: "Exo";
+  color: white;
+  background-color: #d48208;
   display: flex;
-  align-items: center;
   justify-content: center;
-  width: 40px;
-  height: 40px;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-}
-
-.carrito-button {
-  background: none;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-}
-
-.carrito-image {
-  width: 250%;
-  height: 250%;
-  max-width: 30px;
-  max-height: 30px;
-}
-
-.carrito-count {
-  background-color: #fff;
-  color: #e06e43;
-  border-radius: 50%;
-  padding: 4px 8px;
-  font-size: 12px;
-  position: absolute;
-  top: -8px;
-  right: -8px;
-}
-
-.carrito-overlay {
-  position: absolute;
-  top: 100px;
-  left: center;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
   align-items: center;
-  justify-content: center;
-  overflow: auto;
-}
-
-.carrito-container {
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  max-width: 400px;
-  text-align: center;
-}
-
-.cerrar-carrito {
-  background-color: #e06e43;
-  color: #fff;
-  border: none;
-  padding: 8px 16px;
-  font-size: 16px;
-  cursor: pointer;
-  margin-bottom: 10px;
-}
-.salir_pos {
-  position: relative;
-  left: 30%;
 }
 </style>
--- src/pages/PagoReserva.vue
+++ src/pages/PagoReserva.vue
@@ -1,132 +1,62 @@
 <template>
-  <div class="pago-container">
-    <h2>Detalles del Pago</h2>
-    <div class="pago-details">
-      <h3>Resumen de la compra:</h3>
-      <div v-for="item in carrito" :key="item.id" class="carrito-item">
-        <p>{{ item.nombre }}</p>
-        <p>Precio: {{ item.precio }}</p>
-      </div>
-      <h3>Monto total:</h3>
-      <p>{{ montoTotal }}</p>
-    </div>
-    <div class="pago-form">
-      <h3>Ingrese los datos del pago:</h3>
-      <form @submit="realizarPago">
-        <div class="form-group">
-          <label for="nombre">Nombre completo:</label>
-          <input type="text" id="nombre" v-model="nombre" required />
-        </div>
-        <div class="form-group">
-          <label for="tarjeta">Número de tarjeta:</label>
-          <input type="text" id="tarjeta" v-model="numeroTarjeta" required maxlength="16" />
-        </div>
-        <div class="form-group">
-          <label for="vencimiento">Fecha de vencimiento:</label>
-          <input type="date" id="vencimiento" v-model="fechaVencimiento" required />
-        </div>
-        <div class="form-group">
-          <label for="cvv">CVV:</label>
-          <input type="text" id="cvv" v-model="cvv" required maxlength="3" />
-        </div>
-        <button type="submit">Realizar Pago</button>
-      </form>
+  <div class="container">
+    <h5 class="title_cont">RESERVAS</h5>
+    <!-- Resto del código HTML -->
+
+    <div class="payment-container">
+      <h5>Detalles de Pago</h5>
+      <p>Monto Total: {{ montoTotal }}</p>
+      <p>Detalle de la Reserva: {{ reservaDetalle }}</p>
+      <q-btn @click="realizarPago" color="primary">Realizar Pago</q-btn>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      carrito: [], // Aquí se deben colocar los datos del carrito de compras
-      nombre: "",
-      numeroTarjeta: "",
-      fechaVencimiento: "",
-      cvv: "",
-      montoTotal: ""
-    };
-  },
-  created() {
-    this.obtenerMontoTotal();
-  },
-  methods: {
-    calcularTotal() {
-      let total = 0;
-      for (let item of this.carrito) {
-        total += parseFloat(item.precio);
-      }
-      return "$ " + total.toFixed(2);
-    },
-    obtenerMontoTotal() {
-      fetch("http://localhost:5023/api/v1/Pago")
-        .then(response => response.json())
-        .then(data => {
-          this.montoTotal = data.montoTotal;
-        })
-        .catch(error => {
-          console.error("Error al obtener el monto total:", error);
-        });
-    },
-    realizarPago() {
-      // Aquí se pueden agregar acciones para procesar el pago,
-      // como enviar los datos a un servidor o realizar cálculos adicionales.
-      console.log("Pago realizado");
-      // Luego de procesar el pago, se puede redirigir a otra página
-      this.$router.push("/ConfirmacionPago");
-    }
-  }
-};
-</script>
+<style>
+/* Estilos CSS existentes */
 
-<style scoped>
-.pago-container {
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 40px;
-  font-family: "Exo";
-  color: #333;
-  background-color: #eabe7c;
-}
-
-.pago-details {
-  margin-bottom: 40px;
-}
-
-.carrito-item {
-  margin-bottom: 10px;
+.payment-container {
+  margin-top: 20px;
+  border: 1px solid #ccc;
   padding: 10px;
-  background-color: #fff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 8px;
-}
-
-.pago-form {
-  text-align: center;
-}
-
-.form-group {
-  margin-bottom: 20px;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-input {
-  padding: 8px;
-  font-size: 16px;
-}
-
-button {
-  padding: 10px 20px;
-  font-size: 16px;
-  font-weight: bold;
-  color: #fff;
-  background-color: #333;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
 }
 </style>
+
+<script>
+import axios from "axios";
+
+export default {
+  // Resto de las propiedades y métodos existentes
+
+  data() {
+    return {
+      // Resto de las propiedades existentes
+      montoTotal: 0,
+      reservaDetalle: "",
+    };
+  },
+  methods: {
+    // Resto de los métodos existentes
+
+    realizarPago() {
+      const url = "http://localhost:5023/api/v1/Pago";
+
+      const data = {
+        montoTotal: this.montoTotal,
+        reservaDetalle: this.reservaDetalle,
+      };
+
+      axios
+        .post(url, data)
+        .then(response => {
+          // Manejar la respuesta del pago exitoso
+          console.log("Pago realizado correctamente");
+        })
+        .catch(error => {
+          // Manejar el error del pago
+          console.error("Ocurrió un error durante el pago:", error);
+        });
+    },
+  },
+};
+</script>
